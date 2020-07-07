865b8695a7d1d6cdb1a4960e5bc95d0f3cd6d08f
heysushil <sushil.chaudhary111@gmail.com>
2020-07-07 16:20:19 +0530

    about django part 1

---
+++ introduction.py
@@ -0,0 +1,46 @@
+# hello django tarining
+
+'''
+This is Django Class
+
+Django Framwork: MVT
+'''
+
+'''
+Model:
+
+1. Models: models, field types, indexes, meta options, model class
+2. QuerySets: Marking queries, QuerySet method, lookup expressions
+3. Model Instances: instances methos, accessing realted objects
+4. Migration: Migration, operations, schemEdtor, writing mighration
+5. Advaced: Managers, Raw Sql, Transactions, Aggregation, Serch, Custom Fields, Multiple database, Custom lookups, query expressions, coditional expresstion, database function, asynchornous support
+6. Other: Suppored database, optimize database access, PostgreSql specifc features
+'''
+
+'''
+View: Controller
+
+1. basics: URLconfs, view functions, shotcuts, decorators
+2. reference: buil-in views, request/response objects, templateResponse objects
+3. file uploads: file objects, storage api, mangaing files, custom storeage
+4. class-based views: build-in display views, buil-in editing views, api refrence
+5. advacned: generation csv, pdf
+6. Middleware: buil-in middleware classes
+'''
+
+'''
+Template
+
+1. basic
+2. for desingers: languses overviews, buil-in tags and filters
+3. for programmer: template api, custom tags and filters
+'''
+
+'''
+Forms:
+
+1. basic: form api, buil-in fileds, buil-in widgts
+2. advanced: forms for models, interating media, formsets, Customing validation
+'''
+
+
