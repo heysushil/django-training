920275352d136413c323085e4fc6988187a41c78
heysushil <sushil.chaudhary111@gmail.com>
2020-08-25 13:11:29 +0530

    created detail.html page and connect with detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,13 +10,15 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # define a html page in whcih you want to share the data
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {'latest_question_list' : latest_question_list,}
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 # Single question ka detail show karne ke liye method
 def detial(request, question_id):
-    return HttpResponse("You are looking at the question %s" % question_id)
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 # Question ke result ko show karnae ke liye method
 def results(request, question_id):
