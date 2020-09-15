656028914da28d2934e4f1e0f96fcaefc23196a5
heysushil <sushil.chaudhary111@gmail.com>
2020-09-16 00:49:32 +0530

    modifyed on admin panle

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # ye simple question form me date pahle aur question_text ko bad me show karega.
+    # fields = ['pub_date', 'question_text']
+
+    # file set ka use karne section bana 
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+
+# choice ko show karne ke liye admin me register kiya
+admin.site.register(Choice)
