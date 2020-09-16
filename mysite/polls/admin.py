cac177d8729cbd2f2216cfd1a0265ce5be18865b
heysushil <sushil.chaudhary111@gmail.com>
2020-09-16 12:55:14 +0530

    show choices on question form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
+
+# question se realtaed choice show karne ke liye
+'''
+Question form me choices fields show karne ke liye:
+
+1. admin.StackedInline: is ke use se choices fields individual show hoti hain. Jisme ki kafi lines use ho jatei hain. Ya fir ye individual choice section me fields show karta hai.
+
+2. admin.TabularInline: Ye choices ko table form me show karta hai. Is ke use se form chota aur compac ho jata hai.
+'''
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # ye extra 3 qeustion form par 3 blank fiedls dega jisme ki aap choice likh sakte ho.
+    extra = 3
+
+# Question form par date aur text ak section show karene ke liye.
 class QuestionAdmin(admin.ModelAdmin):
     # ye simple question form me date pahle aur question_text ko bad me show karega.
     # fields = ['pub_date', 'question_text']
@@ -12,6 +27,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    # yeha par choices ko fields ko add karna hai
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
